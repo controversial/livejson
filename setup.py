84279b61a4a389c84802e93bf815e05c4c849058
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-06-05 08:41:45 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="livejson",
+    version="0.1",
+    description=("An interface to transparantly bind Python objects to JSON "
+                 "files so that all changes made to the object are reflected "
+                 "in the JSON file"),
+    author="Luke Taylor",
+    author_email="luke@deentaylor.com",
+    url="http://luke.deentaylor.com/",
+)
