0ee2e84a6ca885e0607e36dc0d3c2bdd5ff17cdc
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-06-05 09:00:05 -0400

    Change package description

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name="livejson",
     version="0.1",
-    description=("An interface to transparantly bind Python objects to JSON "
-                 "files so that all changes made to the object are reflected "
-                 "in the JSON file"),
+    description="Bind Python objects to JSON files",
+    long_description=("An interface to transparantly bind Python objects to"
+                      "JSON files so that all changes made to the object are"
+                      "reflected in the JSON file"),
     author="Luke Taylor",
     author_email="luke@deentaylor.com",
     url="http://luke.deentaylor.com/",
