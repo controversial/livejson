2475867b8cb72d923a719d45c1d81942350283f1
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-06-06 17:45:24 -0400

    Move to v1.1.1
    Add keyword for fixing search

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="livejson",
     py_modules=["livejson"],
-    version="1.1.0",
+    version="1.1.1",
     description="Bind Python objects to JSON files",
     long_description=("An interface to transparantly bind Python objects to "
                       "JSON files so that all changes made to the object are "
                       "reflected in the JSON file"),
-    keywords="json io development file files live update",
+    keywords="livejson json io development file files live update",
     license="MIT",
     author="Luke Taylor",
     author_email="luke@deentaylor.com",
